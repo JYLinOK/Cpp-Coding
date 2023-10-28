503c276df03788d42fbd6a44e8dada6f0d36c113
JYLinOK <ydookey@163.com>
2023-10-28 20:15:40 +0800

    void print_type(T value, Targs... args)

--- Types and Objects/ecpp.h
+++ Types and Objects/ecpp.h
@@ -1,4 +1,6 @@
 #include<iostream>
+#define V_NAME(value) (#value)
+
 using namespace std;
 
 void print()
@@ -10,6 +12,7 @@ template<typename T, typename... Targs>
 void print(T value, Targs... args)
 {
     cout << value << ' ';
+
     if ((sizeof ...(args)) > 0)
     {
         print(args...);  // 多个参数时，继续对这些参数使用print()函数
@@ -19,4 +22,29 @@ void print(T value, Targs... args)
         print(); // 无参数时直接输出结束符号。
     }
 }
+
+
+/// <summary>
+/// print the type of a variable
+/// </summary>
+void print_type()
+{
+    cout << endl;
+}
+
+template<typename T, typename... Targs>
+void print_type(T value, Targs... args)
+{
+    cout << V_NAME << " type = " << typedef.id(value).name();
+
+    if ((sizeof ...(args)) > 0)
+    {
+        print_type(args...);  // 多个参数时，继续对这些参数使用print()函数
+    }
+    else
+    {
+        print_type(); // 无参数时直接输出结束符号。
+    }
+}
+
 
